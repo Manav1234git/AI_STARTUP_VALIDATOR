27b5dbe872cd46906cc4cd0bc0f2c7a57dbec9f9
RishiTheHindu <rishithehindu@gmail.com>
2026-04-06 17:26:26 +0530

    path and some views created

--- StartupSense_AI/urls.py
+++ StartupSense_AI/urls.py
@@ -19,6 +19,12 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
       path('', include('core.urls')),
     path('validate/', include('validator.urls')),
+    path('analysis',include('analysis.urls')),
+    path('api',include('api.urls')),
+    path('dashboard',include('dashboard.urls')),
+    path('reports',include('reports.urls')),
+    path('users',include('users.urls')),
 ]
